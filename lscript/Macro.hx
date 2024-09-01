c65f6327f58010eaad73e6fb5eb9d830a6e66886
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-09-01 08:46:52 +0800

    Add files via upload

---
+++ lscript/Macro.hx
@@ -0,0 +1,16 @@
+package lscript;
+
+import haxe.macro.Expr;
+
+
+class Macro {
+	public static macro function loopTable(l:Expr, v:Expr, body:Expr) {
+		return macro {
+			Lua.pushnil($l);
+			while(Lua.next($l, $v < 0 ? $v - 1 : $v) != 0) {
+				$body;
+				Lua.pop($l, 1);
+			}
+		}
+	}
+}
